c0cbe146e0149aa79e278925fbe83aa9cf739c73
DannyPadron09 <danny.padron09@gmail.com>
2021-04-13 15:55:53 -0400

    base game loop structure built

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,22 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome to The Bull Cow Game!!"));
+    PrintLine(TEXT("Welcome to The Bull Cow Game!!")); // TEXT is used so Unreal can encode the string
     PrintLine(TEXT("Input something and press Enter to clear screen"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake"); // TEXT is used so Unreal can encode the string
+    FString HiddenWord = TEXT("cake");
+    if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("You WIN!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You Lose"));
+    }
+
+    // PrintLine(Input);
 }
