6152d2834c4ca978eaf4b94c86471f45a7d4c17b
DannyPadron09 <danny.padron09@gmail.com>
2021-04-18 13:59:47 -0400

    setting up game structure

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -3,16 +3,29 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+
+    // Welcomes the player
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to The Bull Cow Game!!")); // TEXT is used so Unreal can encode the string
     PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
     PrintLine(TEXT("Press Enter to continue..."));
-    HiddenWord = TEXT("cake");
+
+    // Sets up game
+    InitGame();
+    
+
+    
+
+    // Ask user for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    
     ClearScreen();
+
+    //  Checking User guess
+
     if(Input == HiddenWord)
     {
         PrintLine(TEXT("You WIN!"));
@@ -21,4 +34,27 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     {
         PrintLine(TEXT("You Lose"));
     }
+
+    // Check if word is isogram
+    // Check if right number of characters
+
+
+    // Remove life if wrong guess
+
+    // Check to see if Lives are still left
+    // If yes then continue playing
+    // If no then game over
+    // Prompt user if they want to play again 
+
+}
+
+void UBullCowCartridge::InitGame()
+{
+
+    // Sets HiddenWord
+    HiddenWord = TEXT("cake");
+
+    // Set up lives
+
+
 }
