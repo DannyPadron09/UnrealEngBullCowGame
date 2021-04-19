07e02c66867b237321cb833b686ad8079ccf06b7
DannyPadron09 <danny.padron09@gmail.com>
2021-04-19 18:51:31 -0400

    cleaned up code

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -12,40 +12,38 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     // PrintLine(FString::Printf(TEXT("The HiddenWord is: %s"), *HiddenWord));
     // PrintLine(TEXT("The HiddenWord is: %s.\nIt is %i characters long"), *HiddenWord, HiddenWord.Len());
 
-    // Welcomes the player
-    PrintLine(TEXT("Welcome to The Bull Cow Game!!")); // TEXT is used so Unreal can encode the string
-    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); // Magic Number Remove!
-    PrintLine(TEXT("Enter your guess..."));
-
-
-    // Ask user for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
 
-    ClearScreen();
-
-
-
-    //  Checking User guess
-
-    if(Input == HiddenWord)
+    // If game is over then ClearScreen() and SetupGame()
+    // else check User Guess
+    if (bGameOver)
     {
-        PrintLine(TEXT("You WIN!"));
-        // bGameOver = true;
+        ClearScreen();
+        SetupGame();
     }
     else
     {
-        if(Input.Len() == HiddenWord.Len())
+        if(Input == HiddenWord)
         {
-            PrintLine(TEXT("You have the right amount of letters but\nnot correct order"));
+            PrintLine(TEXT("You WIN!"));
+            // bGameOver = true;
         }
         else
         {
-            PrintLine(TEXT("Guess Again"));
-        }
+            if(Input.Len() == HiddenWord.Len())
+            {
+                PrintLine(TEXT("You have the right amount of letters but\nnot correct order"));
+            }
+            else
+            {
+                PrintLine(TEXT("Guess Again"));
+            }
     }
+    }
+    
 
     // Check if word is isogram
     // Check if right number of characters
@@ -65,10 +63,19 @@ void UBullCowCartridge::SetupGame()
 
     // Sets HiddenWord
     HiddenWord = TEXT("cake");
-
     // Set up lives
     Lives = 3;
+    
+    bGameOver = false;
 
-    // bGameOver = false;
+    // Welcomes the player
+    PrintLine(TEXT("Welcome to The Bull Cow Game!!")); // TEXT is used so Unreal can encode the string
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); // Magic Number Remove!
+    PrintLine(TEXT("Enter your guess..."));
 
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
 }
