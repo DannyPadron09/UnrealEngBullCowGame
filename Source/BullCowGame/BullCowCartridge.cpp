7fe7d1d5898b2ea6cc292246cd796a98a9eae65c
DannyPadron09 <danny.padron09@gmail.com>
2021-05-01 12:00:34 -0400

    changed GetValidWords function loop to a range based loop to remove index counting

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -132,11 +132,11 @@ TArray<FString> UBullCowCartridge::GetValidWords(TArray<FString> WordList) const
 
     TArray<FString> ValidWords;
 
-    for (int32 i = 0; i < WordList.Num(); i++)
+    for (FString Word : WordList)
     {
-        if (WordList[i].Len() >= 4 && WordList[i].Len() <= 8 && IsIsogram(Words[i]))
+        if (Word.Len() >= 4 && Word.Len() <= 8 && IsIsogram(Word))
         {
-                ValidWords.Emplace(WordList[i]);
+                ValidWords.Emplace(Word);
         }
     }
 
